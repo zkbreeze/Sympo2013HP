91a5f6ef45e6a348dc3f88a594db77ba3eadc659
siriusjack <siriusjack@gmail.com>
2013-07-24 22:36:04 +0900

    add return_to_top button

--- js/student_script.js
+++ js/student_script.js
@@ -1,9 +1,10 @@
 $(function(){
-	var $nav = $("#side-nav");
-	var nav_width = $nav.width();
-	var offset = $nav.offset().top;
-	var $window = $(window);
+	// side-navスライド設定
 	
+	// var $nav = $("#side-nav");
+	// var nav_width = $nav.width();
+	// var offset = $nav.offset().top;
+	// var $window = $(window);
 	// $window.scroll(function(){
 		// if($window.scrollTop() > offset - 10 && $(window).width() > 780){
 			// $nav.css({
@@ -18,4 +19,25 @@ $(function(){
 			// });
 		// }
 	// });
+	
+	
+	//ホームへ戻るボタン
+	var $return_to_top = $("#return_to_top");
+	var $window = $(window);
+	var offset = 300; //ここまでスクロールしたら表示
+	//スクロール表示処理
+	$window.scroll(function(){
+		if($window.scrollTop() > offset){
+			$return_to_top.fadeIn('slow', function(){});
+		}else{
+			$return_to_top.fadeOut('slow', function(){});
+		}
+	});
+	//クリックで上まで戻る
+	$return_to_top.click(function(){
+		//上に戻る処理
+		$('body,html').animate({
+			scrollTop : 0
+		}, 500);
+	});
 });
